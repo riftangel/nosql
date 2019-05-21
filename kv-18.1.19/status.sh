5904f3069861c5c93c08662fed29f4c6065fa2a8
riftangel <rift.angel@gmail.com>
2019-05-21 05:41:58 -0700

    add admin scripts

---
+++ kv-18.1.19/status.sh
@@ -0,0 +1,1 @@
+java -Doracle.kv.security=kvroot/security/user.security -jar lib/kvstore.jar status -host localhost -port 5000 -root kvroot
---
+++ kv-18.1.19/stop.sh
@@ -0,0 +1,1 @@
+java -Doracle.kv.security=kvroot/security/user.security -jar lib/kvstore.jar stop -host localhost -port 5000 -root kvroot
