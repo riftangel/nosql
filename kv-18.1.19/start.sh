b3b1b6cd8bb9ea2218d9d692177cded1a24c543c
riftangel <rift.angel@gmail.com>
2019-05-21 05:54:36 -0700

    update start.sh

--- kv-18.1.19/start.sh
+++ kv-18.1.19/start.sh
@@ -1,2 +1,1 @@
-nohup java -Doracle.kv.security=/home/ankh/nosql/kv-18.1.19/kvroot/security/user.security -jar lib/kvstore.jar start -host localhost -port 5000 -root kvroot
-java -Doracle.kv.security=/home/ankh/nosql/kv-18.1.19/kvroot/security/user.security -jar lib/kvstore.jar status -host localhost -port 5000 -root kvroot
+nohup java -Doracle.kv.security=kvroot/security/user.security -jar lib/kvstore.jar start -host localhost -port 5000 -root kvroot
